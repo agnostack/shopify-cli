c6ef76b958c42e12fe1baa10e0a99ea91478dcee
Adam Grohs <adam@uniquelyparticular.com>
2023-05-25 16:56:36 -0400

    feat: added proxy url

--- packages/app/src/cli/services/app/update-url.ts
+++ packages/app/src/cli/services/app/update-url.ts
@@ -1,5 +1,5 @@
 import {selectApp} from './select-app.js'
-import {getURLsData, updateURLsData, validatePartnersURLsData, PartnersURLsData} from '../dev/urls.js'
+import {getURLsData, updateURLsData, validatePartnersURLsData, PartnersURLsData, generateProxyURL} from '../dev/urls.js'
 import {allowedRedirectionURLsPrompt, appUrlPrompt} from '../../prompts/update-url.js'
 import {AppProxyUpdate} from '../../api/graphql/app.js'
 import {ensureAuthenticatedPartners} from '@shopify/cli-kit/node/session'
@@ -15,33 +15,39 @@ export interface UpdateURLOptions {
 export default async function updateURLData(options: UpdateURLOptions): Promise<void> {
   const token = await ensureAuthenticatedPartners()
   const apiKey = options.apiKey || (await selectApp()).apiKey
-  const newURLData = await getNewURLData(token, apiKey, {
+  const newURLsData = await getNewURLData(token, apiKey, {
     appURL: options.appURL,
     redirectURLs: options.redirectURLs,
     appProxy: options.appProxy,
   })
-  await updateURLsData(newURLData, apiKey, token)
-  printResult(newURLData)
+  await updateURLsData(newURLsData, apiKey, token)
+  printResult(newURLsData)
 }
 
-// HMMM: add prompt for data
+// HMMM: add prompt for proxy data
 async function getNewURLData(token: string, apiKey: string, options: UpdateURLOptions): Promise<PartnersURLsData> {
-  const currentURLData: PartnersURLsData = await getURLsData(apiKey, token)
-  const newURLData: PartnersURLsData = {
-    applicationUrl: options.appURL || (await appUrlPrompt(currentURLData.applicationUrl)),
+  const currentURLsData: PartnersURLsData = await getURLsData(apiKey, token)
+  const newURLsData: PartnersURLsData = {
+    applicationUrl: options.appURL || (await appUrlPrompt(currentURLsData.applicationUrl)),
     redirectUrlWhitelist:
-      options.redirectURLs || (await allowedRedirectionURLsPrompt(currentURLData.redirectUrlWhitelist.join(','))),
+      options.redirectURLs || (await allowedRedirectionURLsPrompt(currentURLsData.redirectUrlWhitelist.join(','))),
   }
-  validatePartnersURLsData(newURLData)
-  return newURLData
+  validatePartnersURLsData(newURLsData)
+  return newURLsData
 }
 
 function printResult(data: PartnersURLsData): void {
+  const customSections = [
+    {title: 'App URL', body: {list: {items: [data.applicationUrl]}}},
+    {title: 'Allowed redirection URL(s)', body: {list: {items: data.redirectUrlWhitelist}}},
+  ]
+
+  if (data.appProxy) {
+    customSections.push({title: 'App Proxy URL', body: {list: {items: [generateProxyURL(data.appProxy)]}}})
+  }
+
   renderSuccess({
     headline: 'App URLs updated.',
-    customSections: [
-      {title: 'App URL', body: {list: {items: [data.applicationUrl]}}},
-      {title: 'Allowed redirection URL(s)', body: {list: {items: data.redirectUrlWhitelist}}},
-    ],
+    customSections,
   })
 }

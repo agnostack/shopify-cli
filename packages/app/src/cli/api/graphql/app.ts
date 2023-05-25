7044eb0b854ed38d7a451ab107e18218a86151c1
Adam Grohs <adam@uniquelyparticular.com>
2023-05-25 16:18:24 -0400

    fix: added note around subPathPrefix

--- packages/app/src/cli/api/graphql/app.ts
+++ packages/app/src/cli/api/graphql/app.ts
@@ -1,6 +1,8 @@
 export interface AppProxyUpdate {
   url?: string
   subPath?: string
+  // NOTE: AppUpdateInput mutation currently does not support setting subPathPrefix
+  // subPathPrefix?: string
 }
 
 export interface AppProxy {

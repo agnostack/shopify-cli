e42baa4cdab4fb7956b7f99b5618155c7c7131d7
Adam Grohs <adam@uniquelyparticular.com>
2023-05-26 17:44:13 -0400

    feat: abstracted out combineURLParts

--- packages/app/src/cli/utilities/app/app-url.ts
+++ packages/app/src/cli/utilities/app/app-url.ts
@@ -1,11 +1,24 @@
-export function buildAppURLForWeb(storeFqdn: string, publicURL: string) {
+export function buildAppURLForWeb(storeFqdn: string, publicURL: string): string {
   const hostUrl = `${storeFqdn}/admin`
   const hostParam = Buffer.from(hostUrl).toString('base64').replace(/[=]/g, '')
   return `${publicURL}?shop=${storeFqdn}&host=${hostParam}`
 }
 
-export function buildAppURLForMobile(storeFqdn: string, apiKey: string) {
+export function buildAppURLForMobile(storeFqdn: string, apiKey: string): string {
   const hostUrl = `${storeFqdn}/admin/apps/${apiKey}`
   const hostParam = Buffer.from(hostUrl).toString('base64').replace(/[=]/g, '')
   return `https://${hostUrl}?shop=${storeFqdn}&host=${hostParam}`
+}
+
+export function combineURLParts(urlParts: (string | undefined)[], separator = '/'): string {
+  return urlParts
+    .reduce<string[]>((_urlParts, _urlPart = '') => {
+      const urlPart = _urlPart.replace(/^\/|\/$/g, '')
+      if (urlPart) {
+        return [..._urlParts, urlPart]
+      }
+
+      return _urlParts
+    }, [])
+    .join(separator)
 }
